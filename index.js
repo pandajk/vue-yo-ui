b84a87e362b85557be0fcbc5a78915d5e3c71f97
PandaJ <zjarek@me.com>
2018-01-08 18:31:39 +0800

    init vue webpack

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+* @Author: PandaJ
+* @Date:   2017-12-20 10:48:10
+* @Last Modified by:   PandaJ
+* @Last Modified time: 2018-01-08 18:13:15
+*/
+
+
+import YoButton from './src/components/Button';
+
+import YoPopup from './src/components/Popup';
+
+import YoScroller from './src/components/Scroller';
+
+export {
+  YoButton,
+  YoPopup,
+  YoScroller
+}
---
+++ src/components/Button/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="yo-btn" 
+    :class="{
+      'btn-lg': size == 'lg',
+      'btn-md': size == 'md',
+      'btn-sm': size == 'sm'
+      }" 
+    :style="{
+      backgroundColor: backgroundColor
+    }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'YoButton',
+
+  props: {
+    size: {
+      type: String,
+      default: 'md'
+    },
+
+    backgroundColor: {
+      type: String
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../theme.styl';
+.yo-btn
+  background-color $themeColor
+  padding 6px 10px
+  text-align center
+  color #fff
+  border-radius 4px
+  line-height 1em
+  &.disabled
+    background-color #d1d1d1!important
+    color #f5f5f5
+.btn-lg
+  font-size $fontSizeLg
+  padding 14px 18px
+.btn-md
+  font-size $fontSizeMd
+  padding 12px 18px
+.btn-sm
+  font-size $fontSizeSm
+  padding 8px 11px
+</style>
