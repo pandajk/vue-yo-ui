4e13935af6bdbf3b755b2ca6feee2f768a44882a
PandaJ <zjarek@me.com>
2018-01-08 19:35:36 +0800

    init vue webpack

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /*
 * @Author: PandaJ
 * @Date:   2017-12-20 10:48:10
-* @Last Modified by:   panda
-* @Last Modified time: 2018-01-08 17:32:24
+* @Last Modified by:   PandaJ
+* @Last Modified time: 2018-01-08 18:13:15
 */
 
 
-import YoButton from './src/Button';
+import YoButton from './src/components/Button';
 
-import YoPopup from './src/Popup';
+import YoPopup from './src/components/Popup';
 
-import YoScroller from './src/Scroller';
+import YoScroller from './src/components/Scroller';
 
 export {
   YoButton,
