393f37800052eb8215856eba70c219dfd079f98e
PandaJ <zjarek@me.com>
2018-03-16 20:59:28 +0800

    1-test

---
+++ src/components/Test/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Test',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
